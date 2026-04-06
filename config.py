2ef3ef1f547d69b32fedec7cd081c5e53a0d0540
jonze <shobo2excel@gmail.com>
2026-04-06 07:10:09 +0100

    persist chat ID from env variable

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-ALERT_CHAT_IDS = os.getenv("ALERT_CHAT_IDS", "").split(",") if os.getenv("ALERT_CHAT_IDS") else []
+_ids = os.getenv("ALERT_CHAT_IDS", "")
+ALERT_CHAT_IDS = [i.strip() for i in _ids.split(",") if i.strip()] if _ids else []
 SCAN_INTERVAL_MINUTES = int(os.getenv("SCAN_INTERVAL_MINUTES", "10"))
 MIN_ALERT_SCORE = int(os.getenv("MIN_ALERT_SCORE", "65"))
